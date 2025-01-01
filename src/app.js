40457b19428fed1c7add3a4e9010e26c5caac409
Haroon Asif <haroon.asif@tkxel.com>
2025-01-02 01:29:03 +0500

    Worked on Handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello Testing device");
+app.get("/test", (req, res) => {
+    res.send("Get Request");
 });
+app.post("/test", (req, res) => {
+    res.send("Post Request");
+});
+app.put("/test", (req, res) => {
+    res.send("Put Request");
+});
+app.delete("/test", (req, res) => {
+    res.send("Delete Request");
+});
+
+// app.use("/test", (req, res) => {
+//     res.send("Hello Testing device");
+// });
 
 app.listen(2000, () => {
     console.log("Server is successfully running");
